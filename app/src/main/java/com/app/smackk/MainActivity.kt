965bf6d6065ed185199a7032f92cb93a73f08c9b
bob <bob>
2019-11-18 16:46:29 -0500

    Les-1

--- app/src/main/java/com/app/smackk/MainActivity.kt
+++ app/src/main/java/com/app/smackk/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.app.smackk
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -11,20 +13,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(".Main Activity", "First Step On Create")
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
     }
 
+
     override fun onBackPressed() {
+        Log.i(".Main Activity", "OnBacked Pressed")
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
     }
+
+    fun loginBtnNavClicked(view: View){}
+    fun addChanneClickedl(view: View) {}
+    fun sendMessageBtnClicked(view: View) {}
 }
