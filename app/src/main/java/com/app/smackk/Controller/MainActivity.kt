74dab54f10814c388895099178d9a184c8beee03
bob <bob>
2019-11-21 14:28:17 -0500

    les-07

--- app/src/main/java/com/app/smackk/Controller/MainActivity.kt
+++ app/src/main/java/com/app/smackk/Controller/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.app.smackk.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.app.smackk.R
+import com.app.smackk.Services.AuthService
+import com.app.smackk.Services.UserDataService
+import com.app.smackk.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +34,24 @@ class MainActivity : AppCompatActivity() {
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangedReceiver, IntentFilter(
+            BROADCAST_USER_DATA_CHANGE))
     }
+
+    private val userDataChangedReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if(AuthService.isLoggedIn){
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarcolor(UserDataService.avatarColor))
+                loginBtnNavHeaderon.text = "Logout"
+            }
+        }
+    }
+
 
 
     override fun onBackPressed() {
@@ -39,8 +65,23 @@ class MainActivity : AppCompatActivity() {
 
     fun loginBtnNavClicked(view: View){
 
-        val loginIntent = Intent(this,  LoginActivity::class.java)
-        startActivity(loginIntent)
+        if(AuthService.isLoggedIn){
+
+            UserDataService.logout()
+            userNameNavHeader.text = "Please login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeaderon.text = "Login"
+
+
+        }
+        else{
+            val loginIntent = Intent(this,  LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+
 
     }
     fun addChanneClickedl(view: View) {}
